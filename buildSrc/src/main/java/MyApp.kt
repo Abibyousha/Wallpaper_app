4e3469feb51747a2e7089b4446db6154cadd3140
Jul Abib Yousha <114433132+Abibyousha@users.noreply.github.com>
2024-02-27 16:29:36 +0600

    Wallpaper apllication created by me

---
+++ buildSrc/src/main/java/MyApp.kt
@@ -0,0 +1,7 @@
+@file:Suppress("unused")
+
+object MyApp {
+    const val appId = "dev.wacko1805.pixel.wallpapers"
+    const val version = 105
+    const val versionName = "1.0.5"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,19 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "7.0.3"
+    const val kotlin = "1.8.20"
+
+    // OneSignal
+    const val oneSignalPlugin = "0.14.0"
+    const val oneSignal = "4.6.3"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 33
+    const val buildTools = "30.0.3"
+
+    // Frames
+    const val frames = "3.5.4"
+}
